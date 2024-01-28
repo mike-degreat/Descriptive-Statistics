665b73f0bf4cfb19701ddb1a579af8f3ca103433
mike-degreat <okoronum@gmail.com>
2024-01-28 11:50:43 +0100

    a class and a constructor that takes an array of numerical data and stores the data internally

---
+++ statistics.js
@@ -0,0 +1,10 @@
+// takes array of numerical data as input and stores the data internally
+class DescriptiveStatistics {
+    constructor(data) {
+      this.data = data;
+    }
+}
+
+// before calculating the measures of dispersion we need to derive the measure of central tendency because some key concepts in the measures of dispersion are dependent of some factors of measures of central tendency
+
+// Measures of tendency
